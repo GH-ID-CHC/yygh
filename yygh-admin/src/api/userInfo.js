79ffb728b493e92632ab690bfd2cb3df3fee9038
柴和程 <344785479@qq.com>
2024-05-23 22:27:10 +0800

    feat: 用户管理
    用户详情
    用户认证

--- yygh-admin/src/api/userInfo.js
+++ yygh-admin/src/api/userInfo.js
@@ -16,5 +16,19 @@ export default {
       url: `${api_name}/lock/${id}/${status}`,
       method: 'get'
     })
+  },
+  // 用户详情
+  show(id) {
+    return request({
+      url: `${api_name}/show/${id}`,
+      method: 'get'
+    })
+  },
+  // 认证审批
+  approval(id, authStatus) {
+    return request({
+      url: `${api_name}/approval/${id}/${authStatus}`,
+      method: 'get'
+    })
   }
 }
